ada6ef25efe24091967b0785addfa65de33c4e88
Tebogo <tebogo3280359024@outlook.com>
2025-05-14 00:07:18 +0800

    初見雛形

--- src/main/java/com/work/work/config/interceptor/MyInterceptor.java
+++ src/main/java/com/work/work/config/interceptor/MyInterceptor.java
@@ -1,25 +1,25 @@
-package com.work.work.config.interceptor;
-
-import com.work.work.entity.User;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-public class MyInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler)throws Exception{
-        if (request.getRequestURI().equals("/")){
-            return true;
-        }
-        HttpSession session=request.getSession();
-        User user = (User) session.getAttribute("user");
-        if (user != null){
-            return true;
-        }else {
-            session.setAttribute("msg","请先登录！");
-            response.sendRedirect("/user/login");
-            return false;
-        }
-    }
-}
+//package com.work.work.config.interceptor;
+//
+//import com.work.work.entity.User;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//public class MyInterceptor implements HandlerInterceptor {
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler)throws Exception{
+//        if (request.getRequestURI().equals("/")){
+//            return true;
+//        }
+//        HttpSession session=request.getSession();
+//        User user = (User) session.getAttribute("user");
+//        if (user != null){
+//            return true;
+//        }else {
+//            session.setAttribute("msg","请先登录！");
+//            response.sendRedirect("/user/login");
+//            return false;
+//        }
+//    }
+//}
